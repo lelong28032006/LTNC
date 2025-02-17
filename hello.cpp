8dfae30ce654d168479995c3625fb75d9f463481
Long <lelong28032006@icloud.com>
2025-02-17 10:44:01 +0700

    start repositery

---
+++ hello.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include "hello.h"
+
+using namespace std;
+
+int randomNumber() {
+    int ran = rand()%100 + 1;
+    return ran;
+}
+
+int takeNumber() {
+    int n;
+    cout << "Enter a number < 100: ";
+    do {
+        cin >> n;
+    } while (n >= 100);
+    return n;
+}
+
+int PlayerGuess(int ans) {
+    int guess = 0;
+    do {
+        guess = takeNumber();
+        if (guess > ans) {
+            cout << "The number is greater than the answer." << endl;
+        }
+        else if (guess < ans) {
+            cout << "The number is less than the answer." << endl;
+        }
+    }
+    while (guess != ans);
+    cout << "Correct!" << endl;
+    return guess;
+}
+
+// Created by Long on 15/02/2025.
+//
+//
+// Created by Long on 15/02/2025.
+//
---
+++ hello.h
@@ -0,0 +1,11 @@
+int randomNumber();
+int takeNumber();
+int PlayerGuess(int n);
+//
+// Created by Long on 15/02/2025.
+//
+
+#ifndef HELLO_H
+#define HELLO_H
+
+#endif //HELLO_H
